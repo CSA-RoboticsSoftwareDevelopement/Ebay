9f4c636e2d1f4a247d97e7d2f97bb56b304facfc
chandrucsa <chandrue.csa@gmail.com>
2025-04-02 17:45:22 +0530

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,15 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.extensions = ['.ts', '.tsx', '.js', '.json'];
     return config;
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // ✅ Allow images from any domain
+      },
+    ],
+  },
 };
 
 export default nextConfig;
