d0469559c689a2cb87ed068c30d792e0ce5c4ef0
Zeke <130468038+ezekielcoding@users.noreply.github.com>
2025-05-31 09:37:39 +1000

    feat: Enhanced navbar with mobile modal, logo updates, and mobile optimizations better UX new sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'xs': '475px',
+      },
       colors: {
         'primary-black': '#141414',
         'primary-yellow': '#FFC300',
